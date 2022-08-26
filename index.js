88eb06654cd8ed451c627dba7eb0eac06aea7147
Brian Lambert <brianlambert2408@gmail.com>
2022-08-26 14:47:02 +1000

    Completed tasks

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Brian",
+    streetAddress: "123 street"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {
+        ...employee,
+        [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const obj = {...employee};
+    delete obj[key];
+
+    return obj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
